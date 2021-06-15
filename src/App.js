d58685f05261e7aa4ea016a0c819476d044c6a4a
Kim Campos <kim@mediarepublica.com>
2021-06-15 18:28:48 +0800

    use password checker component in app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PasswordChecker from './components/password/passwordChecker';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordChecker/>
+    </div>
+  );
+}
+
+export default App;
